eee6673fdc9a44468b84d7a881ba50cabd57d0d3
BurakHINGE <burakmentese16@gmail.com>
2026-02-04 22:31:04 +0300

    Update MainLibrary.java
    Main menu has been created.

--- MainLibrary.java
+++ MainLibrary.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class MainLibrary {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
         
+        System.out.println("Kütüphaneye Hoş Geldiniz!");
+        System.out.println("1-Giriş Yap\n2-Kayıt Ol\n3-Çıkış Yap");
+        int secenek = input.nextInt();
+
+        while (secenek < 1 || secenek > 3) {
+            System.out.println("Lütfen geçerli bir işlem giriniz!");
+            System.out.println("1-Giriş Yap\n2-Kayıt Ol\n3-Çıkış Yap");
+            secenek = input.nextInt();
+
+        }
+
+        if (secenek == 3) {
+            System.out.println("Kütüphaneden çıkış yapılıyor...");
+            return;
+        }
+        else if (secenek == 2) {
+
+        }
+        else if(secenek == 1) {
+
+        }
+
+
         
 
     }
