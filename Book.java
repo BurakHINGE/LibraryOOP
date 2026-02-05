4ee6a86a5c08fe549f37c98a86fc089cf4f1ec68
BurakHINGE <burakmentese16@gmail.com>
2026-02-05 20:26:05 +0300

    Update Book.java
    Constructor, getter and setter methods have been created.

--- Book.java
+++ Book.java
@@ -1,3 +1,42 @@
 public class Book {
     
+    public String bookName;
+    public String authorName;
+    public String[][] bookList;
+
+    public Book(String bookName, String authorName, String[][] bookList) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.bookList = bookList;
+    }
+
+    
+
+
+
+
+    public String getBookName(String bookName) {
+        return bookName;
+    }
+
+    public String getAuthorName(String authorName) {
+        return authorName;
+    }
+
+    public String[][] getAuthorName(String[][] bookList) {
+        return bookList;
+    }
+
+    public void setBookName() {
+        this.bookName = bookName;
+    }
+
+    public void setAuthorName() {
+        this.authorName = authorName;
+    }
+
+    public void setBookList() {
+        this.bookList = bookList;
+    }
+
 }
