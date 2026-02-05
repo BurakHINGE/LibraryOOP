9bd7452874aa92f83837fc3a0e7d483763526927
BurakHINGE <burakmentese16@gmail.com>
2026-02-05 20:24:51 +0300

    Update User.java
    Constructor, getter and setter methods have been created.

--- User.java
+++ User.java
@@ -1,3 +1,29 @@
 public class User {
+
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String addBooks(String book) {
+        
+    }
+
+    public void listBooks(String[][] bookList) {
+
+    }
+
+    public String getUsername(String username) {
+        return username;
+    }
+
+    public String getPassword(String password) {
+        return password;
+    }
+
+    
     
 }
