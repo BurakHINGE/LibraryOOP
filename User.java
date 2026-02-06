5bd44ad569e4d469852d9e1b56663ac70e706e73
BurakHINGE <burakmentese16@gmail.com>
2026-02-06 18:43:38 +0300

    Improvement
    Some improvements for OOP logic have been updated.

--- User.java
+++ User.java
@@ -1,44 +1,29 @@
+import java.util.ArrayList;
+
 public class User {
 
     private String username;
     private String password;
-    private String[][] bookList = new String[1][2];
-    private Book book;
-    private int librarySize = bookList.length;
+    private ArrayList<Book> bookList;
 
-    public User(String username, String password, String[][] bookList, int librarySize) {
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.bookList = bookList;
-        this.librarySize = librarySize;
+        this.bookList = new ArrayList<>();
     }
 
     public String addBooks(String bookName, String authorName) {
 
-        book = new Book(bookName, authorName);
+        Book newBook = new Book(bookName, authorName);
+        bookList.add(newBook);
 
-        String[][] tempList = new String[librarySize][2];
-
-        for (int i = 0; i < bookList.length; i++) {
-            for (int j = 0; j < bookList[i].length; j++) {
-                tempList[i][j] = bookList[i][j];
-            }
-        }
-
-        tempList[librarySize][0] = bookName;
-        tempList[librarySize][1] = authorName;
-
-        librarySize++;
-
-        return "Book added.";
+        return "Kitabınız kitaplığınıza eklendi.";
     }
 
-    public void listBooks(String[][] bookList) {
+    public void listBooks() {
 
-        for (int i = 0; i < bookList.length; i++) {
-            for (int j = 0; j < bookList[i].length; j++) {
-                System.out.println(bookList[i] + " - " + bookList[i][j]);
-            }
+        for (Book book : bookList) {
+            System.out.println(book.getBookName() + " - " + book.getAuthorName());
         }
     }
 
@@ -50,28 +35,12 @@ public class User {
         return password;
     }
 
-    public String[][] getBookList() {
-        return bookList;
-    }
-
-    public int getLibrarySize() {
-        return librarySize;
-    }
-
     public void setUsername(String username) {
         this.username = username;
     }
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public void setBookList(String[][] bookList) {
-        this.bookList = bookList;
-    }
-
-    public void setLibrarySize(int librarySize) {
-        this.librarySize = librarySize;
     }
     
 }
