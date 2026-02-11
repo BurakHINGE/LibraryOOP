01d64d5aed4778c92f91aea964d1988cc5f7aa50
BurakHINGE <burakmentese16@gmail.com>
2026-02-11 16:21:22 +0300

    Update Library.java
    Register and login methods have been created in Library class.

--- Library.java
+++ Library.java
@@ -1,10 +1,40 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    ArrayList<User> users;
     
     public Library() {
-
+        users = new ArrayList<>();
     }
     
 
+    public boolean register(String username, String password) {
+
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return false;
+            }
+        }
+
+        User newUser = new User(username, password);
+        users.add(newUser);
+        return true;
+    }
+
+    public User login(String username, String password) {
+
+        for (User user : users) {
+
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+
+                return user;
+            }
+
+        }
+
+        return null;
+    }
 
 
 }
