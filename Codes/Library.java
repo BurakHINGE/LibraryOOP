2e9da2003855e7cdacd57eb29f96246cba4434c7
BurakHINGE <burakmentese16@gmail.com>
2026-02-21 23:27:25 +0300

    Update Library.java
    Password requirements method has been enhanced with high protection requirements.

--- Codes/Library.java
+++ Codes/Library.java
@@ -37,13 +37,57 @@ public class Library {
         return null;
     }
 
-    public boolean isAllowedPassword(String password) {
+    public String isAllowedPassword(String password) {
+
+        int capitalLetter = 0;
+        int lowerCaseLetter = 0;
+        int specialCharacter = 0;
+
 
         if (password.length() < 8) {
-            return false;
-        }
 
-        return true;
+            for (int i = 0; i < password.length(); i++) {
+                if (Character.isUpperCase(password.charAt(i))) {
+                    capitalLetter++;
+                }
+                else if (Character.isLowerCase(password.charAt(i))) {
+                    lowerCaseLetter++;
+                }
+                else if (!Character.isLetterOrDigit(password.charAt(i))) {
+                    specialCharacter++;
+                }
+            }
+            
+            if (capitalLetter < 1 && lowerCaseLetter < 1 && specialCharacter < 1) {
+                return "allReqDenied";
+            }
+            else if (capitalLetter < 1 && lowerCaseLetter < 1 && specialCharacter > 0) {
+                return "capitalLower";
+            }
+            else if (capitalLetter < 1 && lowerCaseLetter > 0 && specialCharacter < 1) {
+                return "capitalSpecial";
+            }
+            else if (capitalLetter > 0 && lowerCaseLetter < 1 && specialCharacter < 1) {
+                return "lowerSpecial";
+            }
+            else if (capitalLetter > 0 && lowerCaseLetter > 0 && specialCharacter < 1) {
+                return "special";
+            }
+            else if (capitalLetter > 0 && lowerCaseLetter < 1 && specialCharacter > 0) {
+                return "lower";
+            }
+            else if (capitalLetter < 1 && lowerCaseLetter > 0 && specialCharacter > 0) {
+                return "capital";
+            }
+            else {
+                return "allowed";
+            }
+    
+        }
+        else {
+            return "shortPass";
+        }
+        
     }
 
     public ArrayList<User> getUsers() {
