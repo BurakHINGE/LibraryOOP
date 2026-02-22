3616003e15ce9a553fdede54a99437cd7144c4b5
BurakHINGE <burakmentese16@gmail.com>
2026-02-22 17:18:56 +0300

    Update Library.java
    Bugs have been fixed. Allowerd password method has been updated.

--- Codes/Library.java
+++ Codes/Library.java
@@ -2,6 +2,15 @@ import java.util.ArrayList;
 
 public class Library {
 
+    public static final int SHORT = 0;
+    public static final int ALL_REQ_DENIED = 1;
+    public static final int CAPITAL_LOWER = 2;
+    public static final int CAPITAL_SPECIAL = 3;
+    public static final int LOWER_SPECIAL = 4;
+    public static final int SPECIAL = 5;
+    public static final int LOWER = 6;
+    public static final int CAPITAL = 7;
+    public static final int ALLOWED = 8;
     private ArrayList<User> users;
     
     public Library() {
@@ -37,7 +46,7 @@ public class Library {
         return null;
     }
 
-    public String isAllowedPassword(String password) {
+    public int isAllowedPassword(String password) {
 
         int capitalLetter = 0;
         int lowerCaseLetter = 0;
@@ -45,48 +54,48 @@ public class Library {
 
 
         if (password.length() < 8) {
+            return SHORT;
+        }    
 
-            for (int i = 0; i < password.length(); i++) {
-                if (Character.isUpperCase(password.charAt(i))) {
-                    capitalLetter++;
-                }
-                else if (Character.isLowerCase(password.charAt(i))) {
-                    lowerCaseLetter++;
-                }
-                else if (!Character.isLetterOrDigit(password.charAt(i))) {
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isUpperCase(password.charAt(i))) {
+                capitalLetter++;
+            }
+            else if (Character.isLowerCase(password.charAt(i))) {
+                lowerCaseLetter++;
+            }
+            else if (!Character.isLetterOrDigit(password.charAt(i))) {
                     specialCharacter++;
-                }
             }
+        }
             
-            if (capitalLetter < 1 && lowerCaseLetter < 1 && specialCharacter < 1) {
-                return "allReqDenied";
-            }
-            else if (capitalLetter < 1 && lowerCaseLetter < 1 && specialCharacter > 0) {
-                return "capitalLower";
-            }
-            else if (capitalLetter < 1 && lowerCaseLetter > 0 && specialCharacter < 1) {
-                return "capitalSpecial";
-            }
-            else if (capitalLetter > 0 && lowerCaseLetter < 1 && specialCharacter < 1) {
-                return "lowerSpecial";
-            }
-            else if (capitalLetter > 0 && lowerCaseLetter > 0 && specialCharacter < 1) {
-                return "special";
-            }
-            else if (capitalLetter > 0 && lowerCaseLetter < 1 && specialCharacter > 0) {
-                return "lower";
-            }
-            else if (capitalLetter < 1 && lowerCaseLetter > 0 && specialCharacter > 0) {
-                return "capital";
-            }
-            else {
-                return "allowed";
-            }
-    
+        if (capitalLetter < 1 && lowerCaseLetter < 1 && specialCharacter < 1) {
+            return ALL_REQ_DENIED;
+        }
+        else if (capitalLetter < 1 && lowerCaseLetter < 1 && specialCharacter > 0) {
+            return CAPITAL_LOWER;
+        }
+        else if (capitalLetter < 1 && lowerCaseLetter > 0 && specialCharacter < 1) {
+            return CAPITAL_SPECIAL;
+        }
+        else if (capitalLetter > 0 && lowerCaseLetter < 1 && specialCharacter < 1) {
+            return LOWER_SPECIAL;
+        }
+        else if (capitalLetter > 0 && lowerCaseLetter > 0 && specialCharacter < 1) {
+            return SPECIAL;
+        }
+        else if (capitalLetter > 0 && lowerCaseLetter < 1 && specialCharacter > 0) {
+            return LOWER;
+        }
+        else if (capitalLetter < 1 && lowerCaseLetter > 0 && specialCharacter > 0) {
+            return CAPITAL;
         }
         else {
-            return "shortPass";
+            return ALLOWED;
         }
+    
+        
+        
         
     }
 
